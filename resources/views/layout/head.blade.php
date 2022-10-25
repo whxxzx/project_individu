1dc95defc2a87a86f53bf53dccafec65c9db378c
whxxzx <markona1239@gmail.com>
2022-10-25 11:06:47 +0700

    start commit

--- resources/views/layout/head.blade.php
+++ resources/views/layout/head.blade.php
@@ -7,7 +7,7 @@
 
 
 <!-- Custom fonts for this template-->
-<link href="{{asset('template/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
+<script src="https://kit.fontawesome.com/a071fc050e.js" crossorigin="anonymous"></script>
 <link
     href="{{asset('template/https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i')}}"
     rel="stylesheet">
